2bcef7578236019cdd765897c2a7660dfd7e50e0
Sigap Dwi Aminullah <sigapdwi@outlok.com>
2024-10-28 18:27:24 +0700

    gaskuen

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxy3ISnH05eDfwIMhADHFjftQn6CqtTpWHPVdOk7Aio_uvxZi1-0ViXdLJSpkztgGCs/exec'
 
 const form = document.forms['contact-form']
-
+let namaPengirim = document.getElementById("namaPengirim").value;
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Oke pesan anda kami terima 😚" ))
+  .then(response => alert("Terima kasih, " + namaPengirim ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
 
 
-const textarea = document.querySelector("textarea");
-textarea.addEventListener("keyup",e =>{
-  let scHeight = e.target.scrolHeight;
-  console.log(scHeight)
-})
+// const textarea = document.querySelector("textarea");
+// textarea.addEventListener("keyup",e =>{
+//   // textarea.style.height = "auto";
+//   let scHeight = e.target.scrolHeight;
+//   textarea.style.height = `${scHeight}px`;
+// })
