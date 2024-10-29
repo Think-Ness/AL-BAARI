1f485a1c3620a47ac8066c7d9e7d2decc4f3a52d
Sigap Dwi Aminullah <sigapdwi@outlok.com>
2024-10-29 19:27:34 +0700

    bentras

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxy3ISnH05eDfwIMhADHFjftQn6CqtTpWHPVdOk7Aio_uvxZi1-0ViXdLJSpkztgGCs/exec'
 
-const form = document.forms['contact-form']
-let namaPengirim = document.getElementById("namaPengirim").value;
+let pengirim = document.getElementById("pengirim").value;
+const form = document.forms['contact-form'];
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Terima kasih, " + namaPengirim ))
+  .then(response => alert("Terima kasih, " + pengirim ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
---
+++ js/pengirim.js
@@ -0,0 +1,1 @@
+const pengirim = document.getElementById("pengirim");
